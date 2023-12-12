c3e9f793e2614ca0b1df9ae66cb18e6127058e0b
MAC002 <mac002@MAC002s-Mac-mini.local>
2023-12-12 18:24:10 +0700

    Use library biometric for component

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,52 @@
-import { NativeModules, Platform } from 'react-native';
+import ReactNativeBiometrics, { BiometryTypes } from 'react-native-biometrics';
+export declare const BiometryTypes1: {
+  TouchID: string;
+  FaceID: string;
+  Biometrics: string;
+};
+const BiometricAuthComponent = async (): Promise<any> => {
+  const rnBiometrics = new ReactNativeBiometrics({
+    allowDeviceCredentials: true,
+  });
+  const biometricType = rnBiometrics
+    .isSensorAvailable()
+    .then((resultObject) => {
+      const { available, biometryType } = resultObject;
 
-const LINKING_ERROR =
-  `The package 'react-native-component' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-const Component = NativeModules.Component
-  ? NativeModules.Component
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
+      if (available && biometryType === BiometryTypes.TouchID) {
+        return BiometryTypes.TouchID;
+      } else if (available && biometryType === BiometryTypes.FaceID) {
+        return BiometryTypes.FaceID;
+      } else if (available && biometryType === BiometryTypes.Biometrics) {
+        return BiometryTypes.Biometrics;
+      } else {
+        return 'null';
       }
-    );
+    });
+  return biometricType;
+};
+export const authenticate = () => {
+  const rnBiometrics = new ReactNativeBiometrics({
+    allowDeviceCredentials: true,
+  });
+  rnBiometrics
+    .simplePrompt({
+      promptMessage: 'Confirm fingerprint',
+      fallbackPromptMessage: 'messs',
+      cancelButtonText: 'cancel',
+    })
+    .then((resultObject) => {
+      const { success } = resultObject;
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Component.multiply(a, b);
-}
+      if (success) {
+        console.log('successful biometrics provided');
+      } else {
+        console.log('user cancelled biometric prompt');
+      }
+    })
+    .catch(() => {
+      console.log('biometrics failed');
+    });
+};
+
+export default BiometricAuthComponent;
